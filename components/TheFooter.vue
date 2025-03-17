84483a1ff31c5baa827cf0d5a03e15849ffc5a90
m.verma <m.verma@uplab.ru>
2025-03-17 09:49:06 +0300

    TheFooter mobile

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -1,52 +1,88 @@
 <template>
   <v-footer class="footer" color="#F5F5F5">
-    <div class="d-flex align-center footer-content px-10">
-      <div class="copyright-text">© {{ copyrightText }}</div>
+    <div class="footer-content">
+      <div v-if="!$device.isMobile" class="d-flex align-center w-100">
+        <div class="copyright-text">© {{ copyrightText }}</div>
 
-      <div class="footer-links">
-        <a
-          v-for="(link, index) in links"
-          :key="index"
-          href="#"
-          class="footer-link"
-        >
-          {{ link }}
-        </a>
+        <div class="footer-links">
+          <a
+            v-for="(link, key) in desktopLinks"
+            :key="key"
+            :href="link.href"
+            class="footer-link"
+          >
+            {{ link.text }}
+          </a>
+        </div>
+      </div>
+
+      <div v-else class="mobile-content">
+        <div class="copyright-text mb-4">© {{ copyrightText }}</div>
+
+        <div class="mobile-links">
+          <a
+            v-for="(link, key) in allLinks"
+            :key="key"
+            :href="link.href"
+            class="mobile-link"
+          >
+            {{ link.text }}
+          </a>
+
+          <div class="language-selector">
+            <img src="/icons/ru.svg" alt="Russian" class="language-icon" />
+            <span class="language">Русский</span>
+          </div>
+        </div>
       </div>
     </div>
   </v-footer>
 </template>
 
 <script setup lang="ts">
+import { computed } from "vue";
+
 defineProps({
   copyrightText: {
     type: String,
     default: "Annonce 2023",
   },
-  links: {
-    type: Array,
-    default: () => ["Документы", "О компании"],
-  },
+});
+
+const allLinks = computed(() => {
+  return {
+    documents: { text: "Документы", href: "#" },
+    about: { text: "О компании", href: "#" },
+    sellers: { text: "Продавцам", href: "#" },
+    buyers: { text: "Покупателям", href: "#" },
+    post: { text: "Разместить объявление", href: "#" },
+  };
+});
+
+const desktopLinks = computed(() => {
+  return {
+    documents: allLinks.value.documents,
+    about: allLinks.value.about,
+  };
 });
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 .footer {
   border-top: 1px solid #e0e0e0;
-
-  height: 88px;
+  min-height: 88px;
 }
 
 .footer-content {
   font-family: "Inter", sans-serif;
   font-size: 14px;
   color: #71717a;
-  justify-content: flex-start;
   width: 100%;
 }
 
 .copyright-text {
   font-weight: 500;
+  color: #0f0f0f;
 }
 
 .footer-links {
@@ -57,13 +93,56 @@ defineProps({
   color: #71717a;
   text-decoration: none;
   margin-left: 16px;
+
+  &:first-child {
+    margin-left: 0;
+  }
+
+  &:hover {
+    text-decoration: underline;
+  }
 }
 
-.footer-link:first-child {
-  margin-left: 0;
+/* Mob*/
+.mobile-content {
+  display: flex;
+  flex-direction: column;
+  width: 100%;
 }
 
-.footer-link:hover {
-  text-decoration: underline;
+.mobile-links {
+  display: flex;
+  flex-direction: column;
+}
+
+.mobile-link {
+  color: #71717a;
+  text-decoration: none;
+  padding: 8px 0;
+
+  &:hover {
+    text-decoration: underline;
+  }
+}
+
+.language-selector {
+  display: flex;
+  align-items: center;
+  padding: 8px 0;
+  margin-top: 4px;
+
+  .language-icon {
+    margin-right: 8px;
+    width: 16px;
+    height: 16px;
+  }
+
+  .language {
+    color: #0f0f0f;
+  }
+
+  span {
+    color: #71717a;
+  }
 }
 </style>
